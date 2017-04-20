89695907b5b41f7aa1498ebb37f9faf106fb22e7
Dominick Martelly <dominick.martelly@gmail.com>
2017-04-20 00:23:59 -0400

    Test Commit

--- src/datastructures/stacks/BalancedBrackets.java
+++ src/datastructures/stacks/BalancedBrackets.java
@@ -1,6 +1,7 @@
 package datastructures.stacks;
 
-import java.util.*;
+import java.util.Scanner;
+import java.util.Stack;
 
 /**
  * Created by Dominick Martelly on 4/9/2017 at 8:34 PM.
@@ -13,28 +14,28 @@ public class BalancedBrackets {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int t = in.nextInt();
-        for(int a0 = 0; a0 < t; a0++){
+        for (int a0 = 0; a0 < t; a0++) {
             String s = in.next();
             System.out.println(balanced(s));
         }
         in.close();
     }
 
-    private static String balanced(String s){
+    private static String balanced(String s) {
         Stack<Character> myStack = new Stack<>();
-        for (char c : s.toCharArray()){
-            if (c == '[' || c == '(' || c == '{'){
+        for (char c : s.toCharArray()) {
+            if (c == '[' || c == '(' || c == '{') {
                 myStack.push(c);
             } else {
-                if (myStack.empty()){
+                if (myStack.empty()) {
                     return "NO";
                 }
-                if (myStack.pop() != open(c)){
+                if (myStack.pop() != open(c)) {
                     return "NO";
                 }
             }
         }
-        return myStack.empty() ? "YES":"NO";
+        return myStack.empty() ? "YES" : "NO";
     }
 
     private static char open(char c) {
