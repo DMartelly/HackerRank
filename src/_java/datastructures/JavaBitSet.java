2f989dc270fa2ec7515f18b1768ac444f2edfe9c
Dominick Martelly <dominick.martelly@gmail.com>
2017-04-19 22:27:47 -0400

    Added BitSet

---
+++ src/_java/datastructures/JavaBitSet.java
@@ -0,0 +1,12 @@
+package _java.datastructures;
+
+/**
+ * Created by Dominick Martelly on 4/19/2017 at 10:25 PM.
+ *
+ * @author Dominick Martelly
+ */
+public class JavaBitSet {
+    public static void main(String[] args) {
+
+    }
+}
