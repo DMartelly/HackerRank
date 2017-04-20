48722cd07c7e3c36b805aaa691a5c3bd66897343
Dominick Martelly <dominick.martelly@gmail.com>
2017-04-20 00:23:07 -0400

    Test Commit

--- src/datastructures/stacks/ANDxorOR.java
+++ src/datastructures/stacks/ANDxorOR.java
@@ -1,7 +1,7 @@
 package datastructures.stacks;
 
-import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Scanner;
 
 /**
  * Created by Dominick Martelly on 4/14/2017 at 8:35 PM.
@@ -11,13 +11,13 @@ import java.util.*;
 public class ANDxorOR {
     public static void main(String[] args) {
         int array[];
-        if (args.length == 1){
-           array = new int[5];
-           array[0] = 9;
-           array[1] = 6;
-           array[2] = 3;
-           array[3] = 5;
-           array[4] = 2;
+        if (args.length == 1) {
+            array = new int[5];
+            array[0] = 9;
+            array[1] = 6;
+            array[2] = 3;
+            array[3] = 5;
+            array[4] = 2;
         } else {
             Scanner in = new Scanner(System.in);
             array = new int[in.nextInt()];
@@ -28,27 +28,27 @@ public class ANDxorOR {
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < array.length; i++) {
             for (int j = i + 2; j < array.length + 1; j++) {
-                max = Math.max(max, function(twoSmallest(Arrays.copyOfRange(array,i,j))));
+                max = Math.max(max, function(twoSmallest(Arrays.copyOfRange(array, i, j))));
             }
         }
         System.out.println(max);
     }
 
     private static Tuple twoSmallest(int[] array) {
-        if (array.length < 2){
+        if (array.length < 2) {
             throw new IllegalArgumentException("The array needs to be greater than 1");
         }
-        if (array.length == 2){
+        if (array.length == 2) {
             return new Tuple(array[0], array[1]);
         }
         int smallest, secondSmallest;
         smallest = Math.min(array[0], array[1]);
         secondSmallest = Math.max(array[0], array[1]);
         for (int i = 2; i < array.length; i++) {
-            if (array[i] < smallest){
+            if (array[i] < smallest) {
                 secondSmallest = smallest;
                 smallest = array[i];
-            } else if (array[i] < secondSmallest){
+            } else if (array[i] < secondSmallest) {
                 secondSmallest = array[i];
             }
         }
@@ -59,7 +59,7 @@ public class ANDxorOR {
         return (((tuple.a & tuple.b) ^ (tuple.a | tuple.b)) & (tuple.a ^ tuple.b));
     }
 
-    private static class Tuple{
+    private static class Tuple {
         int a, b;
 
         Tuple(int a, int b) {
