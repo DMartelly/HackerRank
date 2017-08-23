1542ea93437e8a21336f962a2d3f7fa5761cfaca
Dominick Martelly <dominick.martelly@techniatranscat.com>
2017-08-23 13:17:48 -0400

    Started "CamelCase"

---
+++ src/algorithms/strings/CamelCase.java
@@ -0,0 +1,15 @@
+package algorithms.strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by Dominick on 8/23/2017.
+ *
+ * @author Dominick
+ */
+public class CamelCase {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+    }
+}
