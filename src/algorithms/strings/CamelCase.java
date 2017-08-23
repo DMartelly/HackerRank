ca4bb33f28ad695f087ddec5caff8bb46f715cae
Dominick Martelly <dominick.martelly@techniatranscat.com>
2017-08-23 13:23:37 -0400

    Solved "CamelCase"

--- src/algorithms/strings/CamelCase.java
+++ src/algorithms/strings/CamelCase.java
@@ -2,6 +2,8 @@ package algorithms.strings;
 
 import java.util.Scanner;
 
+import static java.lang.Character.isUpperCase;
+
 /**
  * Created by Dominick on 8/23/2017.
  *
@@ -11,5 +13,10 @@ public class CamelCase {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         String s = in.next();
+        int result = 1;
+        for (char c : s.toCharArray()) {
+            if (isUpperCase(c)) result++;
+        }
+        System.out.println(result);
     }
 }
