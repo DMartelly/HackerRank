dd282f904259b1453cf304f91c03348b9b99ec01
Dominick Martelly <dominick.martelly@techniatranscat.com>
2017-08-23 13:27:43 -0400

    Started "Two Characters"

---
+++ src/algorithms/strings/TwoCharacters.java
@@ -0,0 +1,16 @@
+package algorithms.strings;
+
+import java.util.Scanner;
+
+/**
+ * Created by Dominick on 8/23/2017.
+ *
+ * @author Dominick
+ */
+public class TwoCharacters {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int len = in.nextInt();
+        String s = in.next();
+    }
+}
